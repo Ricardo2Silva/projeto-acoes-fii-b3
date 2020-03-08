094537f2feb8635bf9da6d066a53978bdcec51d7
Ricardo2Silva <ricardojonasdasilva60@gmail.com>
2020-03-07 21:44:48 -0300

    Subindo primeiras alteraçoes

---
+++ app/database/insert açoes-fii.sql
@@ -0,0 +1,8 @@
+INSERT INTO ACAO(nome_empresa,ticket,segmento) VALUES('Itausa','ITSA4','Bancario');
+INSERT INTO ACAO(nome_empresa,ticket,segmento) VALUES('Maxi Renda','MXRF11','FII');
+INSERT INTO ACAO(nome_empresa,ticket,segmento) VALUES('Grand Plaza Shopping ','ABCP11',' FII Shopping');
+INSERT INTO ACAO(nome_empresa,ticket,segmento) VALUES('Centro Textil','CTXT11','FII');
+INSERT INTO ACAO(nome_empresa,ticket,segmento) VALUES('Taesa','TAEE4','Eletrico');
+INSERT INTO ACAO(nome_empresa,ticket,segmento) VALUES('SDI Logistica','SDIL11','FII Logistica');
+
+select * from ACAO;
